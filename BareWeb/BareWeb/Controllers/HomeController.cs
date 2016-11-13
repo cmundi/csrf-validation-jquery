5b8fe12a924ae9c9732463eaf4c9dbf0442bc410
C. Mundi <cmundi@gmail.com>
2016-11-13 10:41:11 -0700

    Add files via upload

---
+++ BareWeb/BareWeb/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BareWeb.Models;
+
+namespace BareWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult Test(Thing thing)
+        {
+            if (thing.Age > 0)
+                return Json("You rock!");
+            else
+                return Json("Age must be positive!");
+        }
+    }
+}
---
+++ BareWeb/BareWeb/Models/Thing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BareWeb.Models
+{
+    public class Thing
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Remember { get; set; }
+        public List<int> Selections { get; set; }
+        // N.B. There is no "Auth" property, so no amount of malice can bind an Auth value.
+    }
+}
